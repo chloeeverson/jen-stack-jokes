80ecc4b45af16f9d2805f0a774b7109c8f9c19a3
Chloe Everson <chloeliz93@gmail.com>
2021-04-19 11:11:17 -0500

    added function getJokes so new joke can go into request body and array will be passed in rendor method to display once created rendor function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -45,3 +45,22 @@ function addJoke() {
     $('#punchlineIn').val('');
 }//end function addJoke
 
+function getJokes(){
+    $.ajax({
+        method: 'GET',
+        url: '/jokes',
+        //new joke going in request body
+    })
+        .then( function(response) {
+            console.log('response from server' , response);
+            //pass array into rendor method to display
+            render(response);
+        })
+        .catch( function(error) {
+            console.log('error from server' , error);
+            alert('sorry, could not add your joke. try again later.')
+        })
+    console.log('after making server request...');
+    
+}//end function getJokes
+
